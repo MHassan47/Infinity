8abb08c4d1b18aa971b5a972ec5dc3cef7e5ed2c
Mohamed <mhassankd35@gmail.com>
2023-04-14 10:58:19 -0500

    creates firebase logic to get recipe items

--- screens/RecipesScreen.jsx
+++ screens/RecipesScreen.jsx
@@ -1,11 +1,30 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 const RecipesScreen = () => {
+  const [recipe, setRecipe] = useState(null);
+  const itemRef = collection(db, "Items");
+  useEffect(() => {
+    const getRecipe = async () => {
+      const item = [];
+      const data = await getDocs(itemRef);
+      data.forEach((doc) => {
+        item.push({
+          ...doc.data(),
+          id: doc.id,
+        });
+      });
+      setRecipe(item);
+    };
+    getRecipe();
+  }, []);
+
+  console.log(recipe);
   return (
-    <View>
+    <SafeAreaView>
       <Text>RecipesScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
