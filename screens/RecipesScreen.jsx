14ef4852e59ab5e5d6a44eb1d8f4a7f2e58a2ab7
Mohamed <mhassankd35@gmail.com>
2023-04-14 12:53:34 -0500

    renders list of recipe items

--- screens/RecipesScreen.jsx
+++ screens/RecipesScreen.jsx
@@ -1,29 +1,78 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { collection, getDocs } from "firebase/firestore";
-const RecipesScreen = () => {
-  const [recipe, setRecipe] = useState(null);
-  const itemRef = collection(db, "Items");
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { MaterialIcons, Feather } from "@expo/vector-icons";
+const RecipesScreen = ({ route }) => {
+  const { category } = route.params;
+  //   console.log("----------", typeof category);
+  const [recipes, setRecipes] = useState([]);
+  const recipeListRef = collection(db, "Items");
   useEffect(() => {
-    const getRecipe = async () => {
-      const item = [];
-      const data = await getDocs(itemRef);
-      data.forEach((doc) => {
-        item.push({
+    const getRecipeList = async () => {
+      const items = [];
+      const q = query(recipeListRef, where("category", "==", category));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // console.log(doc);
+        items.push({
           ...doc.data(),
-          id: doc.id,
+          title: doc.id,
         });
       });
-      setRecipe(item);
+      setRecipes(items);
     };
-    getRecipe();
+    getRecipeList();
   }, []);
 
-  console.log(recipe);
+  console.log(recipes[1]);
   return (
     <SafeAreaView>
-      <Text>RecipesScreen</Text>
+      <ScrollView
+        className="h-full"
+        contentContainerStyle={{ paddingBottom: 30 }}
+        showsVerticalScrollIndicator={false}
+      >
+        {recipes.map((item) => (
+          <TouchableOpacity
+            key={item.title}
+            className="flex justify-center items-center w-full mb-4 shadow-2xl"
+          >
+            <Image
+              source={{ uri: item.image }}
+              className="w-5/6 h-52 rounded-xl "
+            />
+            <View className="flex absolute bottom-0 left-50 w-5/6 h-full bg-black opacity-50  rounded-xl p-2">
+              <Text className="text-white font-semibold text-lg">
+                {item.title}
+              </Text>
+              <View className="flex flex-row items-center space-x-2">
+                <MaterialIcons
+                  name="local-fire-department"
+                  size={22}
+                  color="white"
+                />
+                <Text className="text-white font-semibold text-lg">
+                  {item.calories}
+                </Text>
+              </View>
+              <View className="flex flex-row items-center space-x-2">
+                <Feather name="clock" size={22} color="white" />
+                <Text className="text-white font-semibold text-lg">
+                  {item.time}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
