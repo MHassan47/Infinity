8d6776de8834d4855c1fc77043b0eebc58914a2a
Mohamed <mhassankd35@gmail.com>
2023-04-14 21:56:29 -0500

    adds conditional rendering of app or auth stack

--- Routes.jsx
+++ Routes.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppTabs } from "./navigators/AppTabs";
+import { AuthStack } from "./navigators/AuthStack";
+import { UserContext } from "./userContext";
 
 // import AppStack from "./navigators/AppStack";
 // import { AuthStack } from "./navigators/AuthStack";
 
 const Routes = () => {
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <NavigationContainer>
-      <AppTabs />
+      {user ? <AppTabs /> : <AuthStack />}
     </NavigationContainer>
   );
 };
