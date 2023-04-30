7dce44d052590ed7dc5e9bf6664705fc1af3f4fd
Mohamed <mhassankd35@gmail.com>
2023-04-29 23:47:27 -0500

    creates profile page screen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,11 +1,22 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useContext } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import { UserContext } from "../userContext";
 const ProfileScreen = () => {
+  const { user } = useContext(UserContext);
+
+  const handleLogout = () => {
+    logout();
+    navigation.navigate("Login");
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 mt-20 items-center space-y-4">
+      <View className="flex justify-center items-center border p-4 w-1/4 rounded-3xl">
+        <FontAwesome name="user" size={60} color="#005a84" />
+      </View>
+      <Text className="text-xl font-semibold ">{user.name}</Text>
+    </SafeAreaView>
   );
 };
 
